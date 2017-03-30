fa37c5e6a0c32319bfcdd04cfc5e48123cb71ed8
Daniel Galvão <daniel.galvao@justdigital.com.br>
2017-03-30 10:00:55 -0300

    Add query-int config

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,13 +1,17 @@
 const port = 3003
 
-const bodyParser = require('body-parser')
-const express    = require('express')
-const server     = express()
-const allowCors       = require('./cors')
+const bodyParser  = require('body-parser')
+const express     = require('express')
+const server      = express()
+const allowCors   = require('./cors')
+const queryParser = require('express-query-int')
 
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
-server.use(allowCors)
+server
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .use(allowCors)
+  .use(queryParser)
+
 
 server.listen(port, function(){
   console.log(`Backend is running on port ${port}`)
