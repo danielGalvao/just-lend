557cedfba969aebae3b9f2f5a764dfac1040c985
Daniel Galvão <daniel.galvao@justdigital.com.br>
2017-03-29 23:27:00 -0300

    Add counter for objects with route

--- backend/src/api/justLend/objetoService.js
+++ backend/src/api/justLend/objetoService.js
@@ -3,4 +3,14 @@ const objetoService = require('./objeto')
 objetoService.methods(['get', 'post', 'put', 'delete'])
 objetoService.updateOptions({new: true, runValidators: true})
 
+objetoService.route('count', (req, res, next) => {
+  objetoService.count((error, value) => {
+    if(error) {
+      res.status(500).json({errors: [error]})
+    } else {
+      res.json({value})
+    }
+  })
+})
+
 module.exports = objetoService
