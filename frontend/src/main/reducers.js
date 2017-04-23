84f8248b73bb0e2e12407df9069a3eb065a5e318
Daniel Galvão <galvaodev@gmail.com>
2017-04-23 01:34:25 -0300

    Add redux form and starting this

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 import objectReducer from '../objects/objectReducer'
 
 const rootReducer = combineReducers({
-  object: objectReducer
+  object: objectReducer,
+  form: formReducer
 })
 
 export default rootReducer
