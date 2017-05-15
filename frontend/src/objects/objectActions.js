a495576321a7fd72d9753265734521980620a38f
Daniel Galvão <daniel.galvao@justdigital.com.br>
2017-05-15 12:50:53 -0300

    Add commom function submit to use creating and updating

--- frontend/src/objects/objectActions.js
+++ frontend/src/objects/objectActions.js
@@ -1,19 +1,41 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
 
-const URL = 'http://localhost:3003/api/objetos'
+const URL = 'http://localhost:3003/api'
 
 export function create(values) {
-  axios.post(`${URL}`, values)
+  values.type = '58f2d3fafefb22482d10613e'
+  const params = {
+    'method': 'post',
+    'msg': 'Objeto cadastrado com sucesso!'
+  }
+  submit(values, params)
+  return {
+    type: 'CREATE_OBJECT'
+  }
+}
+
+export function update(values) {
+  const params = {
+    'method': 'put',
+    'msg': 'Objeto atualizado com sucesso!'
+  }
+  submit(values, params)
+  return {
+    type: 'UPDATE_OBJECT'
+  }
+}
+
+function submit(values, params) {
+
+  const id = values._id ? values._id : ''
+  axios[params.method](`${URL}/objetos/${id}`, values)
     .then(resp => {
-      toastr.success('Sucesso', 'Objeto cadastrado com sucesso!')
+      toastr.success('Sucesso', params.msg)
     })
     .catch(err => {
       err.response.data.errors.map(
         e => toastr.error('Erro', e)
       )
     })
-  return {
-    type: 'CREATE_OBJECT'
-  }
 }
