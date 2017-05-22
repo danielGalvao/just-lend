fc5804a027c79b32c83eb48ff0b286bcc146a6b5
Daniel Galvão <daniel.galvao@justdigital.com.br>
2017-05-22 10:46:20 -0300

    Fix texts

--- frontend/src/templates/head.jsx
+++ frontend/src/templates/head.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default props => (
   <header className="jumbotron hero-spacer">
       <h1>Apenas Empreste!</h1>
-      <p>Essa aplicação foi pensada apenas para facilitar a divulgação, emprestimo e busca de objetos. Nunca se sabe quando você vai precisar de algo. Muito menos de quem possui isso.</p>
+      <p>Essa aplicação foi pensada apenas para facilitar a divulgação, emprestimo e busca de objetos. Nunca se sabe quando você vai precisar de algo. Muito menos de quem possui o que você precisa.</p>
       <p><a href="#/add" className="btn btn-primary btn-large">Cadatre agora!</a>
       </p>
   </header>
