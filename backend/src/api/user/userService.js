7e1d5dd69a5aa3573cc415c7a8092680e17f1542
Daniel Galvão <galvaodev@gmail.com>
2017-05-27 17:31:03 -0300

    Improvind user service exporting modules to login, signup and token

--- backend/src/api/user/userService.js
+++ backend/src/api/user/userService.js
@@ -1,11 +1,11 @@
-const _ = require('lodash')
-const jwt = require('jsonwebtoken')
+const _      = require('lodash')
+const jwt    = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
-const User = require('./user')
-const env = require('../../.env')
+const User   = require('./user')
+const env    = require('../../.env')
 
-const emailRgx = /\S+@\S+\.\S+/
-const passwdRgx = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
+const emailRgx  = /\S+@\S+\.\S+/
+const passwdRgx = /((?=.*\d)(?=.*[0-9])(?=.*[A-Z]).{6,20})/
 
 const sendErrorsDB = (res, dbErrors) => {
   const errors = []
@@ -13,7 +13,73 @@ const sendErrorsDB = (res, dbErrors) => {
   return res.status(400).json({errors})
 }
 
-userService.methods(['get', 'post', 'put', 'delete'])
-userService.updateOptions({new: true, runValidators: true})
+const login = (req, res, next) => {
+  const email = req.body.email || ''
+  const passwd = req.body.passwd || ''
 
-module.exports = userService
+  User.findOne({ email }, (err, user) => {
+    if(err) {
+      return sendErrorsDB(res, err)
+    } else if(user && bcrypt.compareSync(passwd, user.passwd)) {
+      const token = jwt.sign(user, env.authSecret, {
+        expireIn: "1 day"
+      })
+      const {name, email} = user
+      res.json({name, email, token})
+    } else {
+      return res.status(400).send({errors: ['Usuário/Senha inválidos.']})
+    }
+  })
+}
+
+const validateToken = (req, res, next) => {
+  const token = req.body.token || ''
+
+  jwt.verify(token, env.authSecret, (err, decoded) => {
+    return res.status(200).send({ valid: !err})
+  })
+}
+
+const signup = (req, res, next) => {
+
+  const name        = req.body.name || ''
+  const email       = req.body.email || ''
+  const passwd      = req.body.passwd || ''
+  const checkPasswd = req.body.checkPasswd || ''
+
+  if(!email.match(emailRgx)) {
+    return res.status(400).send({errors: ['O email informado está inválido.']})
+  }
+
+  if(!passwd.match(passwdRgx)) {
+    return res.status(400).send({
+      errors: ['Senha precisa ter: uma letra maiúscula, um número e ter entre 6 e 20 caracteres.']
+    })
+  }
+
+  const salt = bcrypt.genSaltSync()
+  const passwdHash = bcrypt.hashSync(passwd, salt)
+
+  if(!bcrypt.compareSync(checkPasswd, passwdHash)) {
+    return res.status(400).send({ errors: ['Senhans não conferem.']})
+  }
+
+  User.findOne({email}, (err, user) => {
+    if(err) {
+      return sendErrorsDB(res, err)
+    } else if(user) {
+      return res.status.send({errors: ['Usuário já cadastrado.']})
+    } else {
+      const newUser = new User({ name, email, passwd: passwdHash})
+      newUser.save(err => {
+        if(err) {
+          return sendErrorsDB(res, err)
+        } else {
+          login(req, res, next)
+        }
+      })
+    }
+  })
+}
+
+module.exports = {login, signup, validateToken}
